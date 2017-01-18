e5b75de3f83bd9b4de6a43209182034ce99af34b
haoxiangzhx <haoxiangzhx@gmail.com>
2017-01-17 19:58:26 -0800

    try test fixtures

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,7 +1,9 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <iostream>
+#include <string>
 
-TEST(NginxConfigParserTest, SimpleConfig1) {
+TEST(NginxConfigTest, SimpleConfig0) {
   NginxConfigParser parser;
   NginxConfig out_config;
 
@@ -10,42 +12,40 @@ TEST(NginxConfigParserTest, SimpleConfig1) {
   EXPECT_TRUE(success);
 }
 
-TEST(NginxConfigParserTest, MissingStartBlock) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
-  std::stringstream stream("server \n {listen 80;");
+class NginxConfigParserTest : public ::testing::Test {
+protected:
+	bool ParseFile(const char* file_name){
+		return parser_.Parse(file_name, &out_config_);
+	}
+	bool ParseString(const std::string config_string) {
+		std::stringstream config_stream(config_string);
+		return parser_.Parse(&config_stream, &out_config_);
+	}
+	NginxConfigParser parser_;
+	NginxConfig out_config_;
+};
 
-  bool success = parser.Parse(&stream, &out_config);
+TEST_F(NginxConfigParserTest, SimpleConfig1){
+	EXPECT_TRUE(ParseFile("example_config"));
+}
 
+TEST_F(NginxConfigParserTest, MissingStartBlock) {
+  bool success = ParseString("server \n {listen 80;");
   EXPECT_FALSE(success);
 }
 
-TEST(NginxConfigParserTest, MissingEndBlock) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
-  std::stringstream stream("server \n listen 80; }");
-
-  bool success = parser.Parse(&stream, &out_config);
-
+TEST_F(NginxConfigParserTest, MissingEndBlock) {
+  bool success = ParseString("server \n listen 80; }");
   EXPECT_FALSE(success);
 }
 
-TEST(NginxConfigParserTest, SimpleConfig2) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
-  std::stringstream stream("server \n { listen {80;} \n server_name {foo.com;} \n root /home/ubuntu/sites/foo/;}");
-
-  bool success = parser.Parse(&stream, &out_config);
-
+TEST_F(NginxConfigParserTest, SimpleConfig2) {
+  bool success = ParseString("server \n { listen {80;} \n server_name {foo.com;} \n root /home/ubuntu/sites/foo/;}");
   EXPECT_TRUE(success);
+  EXPECT_EQ(1, out_config_.statements_.size());
 }
 
-TEST(NginxConfigParserTest, LastNestedBlockError) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
-  std::stringstream stream("server \n { listen {80;} \n server_name {foo.com;} \n root {/home/ubuntu/sites/foo/;}}");
-
-  bool success = parser.Parse(&stream, &out_config);
-
+TEST_F(NginxConfigParserTest, LastNestedBlockError) {
+  bool success = ParseString("server \n { listen {80;} \n server_name {foo.com;} \n root {/home/ubuntu/sites/foo/;}}");
   EXPECT_TRUE(success);
 }
